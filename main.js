eaab85af5bcd6af4ec1d248a0dc1b5725d41a380
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-11 16:35:55 +0200

    modify main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let number = 1;
 const addElement = function () {
     const divElement = document.createElement('div');
     divElement.textContent = number;
+    
+    if(number%5 == 0) {
+        divElement.classList.add('circle')
+    }
     document.body.appendChild(divElement); 
     number++; 
 }
