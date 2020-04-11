8b1112ccace0e149bff6e664438c76971dfe3d5f
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-11 17:40:22 +0200

    next part exercise in main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const btn = document.querySelector('button');
+const btn = document.querySelector('.btn');
 let number = 1;
 
 const addElement = function () {
     const divElement = document.createElement('div');
     divElement.textContent = number;
-    
+
     if(number%5 == 0) {
         divElement.classList.add('circle')
     }
@@ -13,3 +13,21 @@ const addElement = function () {
 }
 
 btn.addEventListener('click', addElement);
+
+
+const btn1 = document.querySelector('.btn1');
+
+
+const addNumber = function(){
+    const ulAdd = document.querySelector('ul');
+    const ulElement = document.createElement('li');
+    ulElement.textContent = number;
+    
+    if(number%3 == 0) {
+        ulElement.classList.add('big');
+    }
+    ulAdd.appendChild(ulElement);
+    number = number + 2;
+}
+
+btn1.addEventListener('click', addNumber);
